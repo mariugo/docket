29abf74c0b9e1d2123638c9e1b9353ba088b4dca
Mariugo Megiani <mariugomegiani@gmail.com>
2022-02-11 18:04:49 -0300

    ChipAddWidget initial imp

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 import 'package:docket/data/local/task_repository.dart';
@@ -6,17 +7,29 @@ import '../../data/model/task.dart';
 
 class HomeController extends GetxController {
   final TaskRepository taskRepository;
+  final formKey = GlobalKey<FormState>();
+  final editController = TextEditingController();
+  final tasks = <Task>[].obs;
+  final chipIndex = 0.obs;
 
   HomeController({
     required this.taskRepository,
   });
-
-  final tasks = <Task>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     tasks.assignAll(taskRepository.readTasks());
     ever(tasks, (_) => taskRepository.writeTasks(tasks));
+  }
+
+  @override
+  void dispose() {
+    editController.dispose();
+    super.dispose();
+  }
+
+  void changeChipIndex(int index) {
+    chipIndex.value = index;
   }
 }
--- lib/modules/home/widgets/add_card_widget.dart
+++ lib/modules/home/widgets/add_card_widget.dart
@@ -20,7 +20,67 @@ class AddCardWidget extends StatelessWidget {
       height: squareWidth / 2,
       margin: EdgeInsets.all(3.0.deviceWidth),
       child: InkWell(
-        onTap: () {},
+        onTap: () async {
+          await Get.defaultDialog(
+            titlePadding: EdgeInsets.symmetric(
+              vertical: 5.0.deviceWidth,
+            ),
+            radius: 5.0,
+            title: 'Task Type',
+            content: Form(
+              key: homeController.formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 3.0.deviceWidth,
+                    ),
+                    child: TextFormField(
+                      controller: homeController.editController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Title',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter your task title';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5.0.deviceWidth,
+                    ),
+                    child: Wrap(
+                      spacing: 2.0.deviceWidth,
+                      children: icons
+                          .map(
+                            (_icon) => Obx(() {
+                              final index = icons.indexOf(_icon);
+                              return ChoiceChip(
+                                selectedColor: Colors.grey[200],
+                                pressElevation: 0,
+                                backgroundColor: Colors.white,
+                                label: _icon,
+                                selected:
+                                    homeController.chipIndex.value == index,
+                                onSelected: (bool selected) {
+                                  homeController.chipIndex.value =
+                                      selected ? index : 0;
+                                },
+                              );
+                            }),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
         child: DottedBorder(
           color: Colors.grey[400]!,
           dashPattern: const [8, 4],
