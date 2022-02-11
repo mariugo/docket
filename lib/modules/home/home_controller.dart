dbc44f9266359b056ed04fd8cf4b01fc6c4520db
Mariugo Megiani <mariugomegiani@gmail.com>
2022-02-11 16:39:56 -0300

    HomeView List initial

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -2,10 +2,21 @@ import 'package:get/get.dart';
 
 import 'package:docket/data/local/task_repository.dart';
 
+import '../../data/model/task.dart';
+
 class HomeController extends GetxController {
   final TaskRepository taskRepository;
 
   HomeController({
     required this.taskRepository,
   });
+
+  final tasks = <Task>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    tasks.assignAll(taskRepository.readTasks());
+    ever(tasks, (_) => taskRepository.writeTasks(tasks));
+  }
 }
--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -2,15 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '/modules/home/home_controller.dart';
+import '/core/utils/extensions.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home View'),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'My List',
+                style: TextStyle(
+                  fontSize: 24.0.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
