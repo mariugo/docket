0b1108c7867eb9a078d7abcb6a0ff87f60bdfeb7
Mariugo Megiani <mariugomegiani@gmail.com>
2022-02-11 12:02:47 -0300

    TaskProvider writeTasks imp

---
+++ lib/data/providers/task_provider.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+
+import '/data/local/data_store_service.dart';
+import '/core/values/keys.dart';
+import '/data/model/task.dart';
+
+class TaskProvider {
+  final _dataStoreService = Get.find<DataStoreService>();
+
+  void writeTaks(List<Task> tasks) {
+    _dataStoreService.write(taskKey, jsonEncode(tasks));
+  }
+
+  List<Task> readTasks() {
+    var tasks = <Task>[];
+    jsonDecode(_dataStoreService.read(taskKey).toString())
+        .forEach((task) => tasks.add(Task.fromJson(task)));
+    return tasks;
+  }
+}
